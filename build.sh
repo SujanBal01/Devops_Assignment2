6957bddc2d46f1584a4f0391d9f3fa118aba6511
SujanBal01 <107382374+SujanBal01@users.noreply.github.com>
2023-12-13 19:49:10 +1030

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
-echo "Running tests..."
-python -m unittest discover
-echo "Creating deployable application package..."
-pip install pyinstaller
-pyinstaller --onefile Addition.py
+Write-Host "Running tests..."
+
+python -m unittest test.py
+
+if ($LASTEXITCODE -eq 0) {
+    Write-Host "Tests passed. Creating deployable application package..."
+
+    pip install pyinstaller
+    pyinstaller --onefile Addition.py
+} else {
+    Write-Host "Tests failed. Fix the issues before creating the application package."
+    exit 1
+}
