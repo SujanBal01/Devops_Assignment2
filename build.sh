5a56bd6ce644e7d2555f6a1086315117fe77156b
SujanBal01 <107382374+SujanBal01@users.noreply.github.com>
2023-12-13 19:39:34 +1030

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ echo "Running tests..."
 python -m unittest discover
 echo "Creating deployable application package..."
 pip install pyinstaller
-pyinstaller --onefile SimplePythonProgram.py
+pyinstaller --onefile Addition.py
